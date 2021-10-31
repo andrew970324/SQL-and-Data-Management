6cd956ae44b03d95da1fea1a8f126fa68c0e7a40
Andrew Wang <71611878+andrew970324@users.noreply.github.com>
2021-10-30 22:25:58 -0700

    Update q8.sql

--- q8.sql
+++ q8.sql
@@ -1,1 +1,5 @@
-SELECT name FROM Student WHERE id NOT IN (SELECT stud_id FROM Takes WHERE (semester = 'Spring' AND year = '2009') OR (semester = 'Spring' AND year = '2010'));
+SELECT name 
+FROM Student 
+WHERE id NOT IN (SELECT stud_id FROM Takes WHERE (semester = 'Spring' AND year = '2009') 
+                 OR 
+                 (semester = 'Spring' AND year = '2010'));
